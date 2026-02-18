ad68857eab8c7ddb2419064a39d5ac7ef78b2bfb
Zetacode2007 <fernandozeta057@gmail.com>
2026-02-18 14:27:40 -0600

    usuarios.routes subido

---
+++ src/routes/usuario.routes.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import {
+    createUsuario,
+    getUsuarios,
+    getUsuarioById,
+    searchUsuario,
+    updateUsuario,
+    deleteUsuario
+} from "../controllers/usuario.controller.js";
+
+const route = Router();
+
+route.post(
+    "/:create",
+    createUsuario
+);
+
+route.get(
+    "/",
+    getUsuarios
+);
+
+route.get(
+    "/search",
+    searchUsuario
+);
+
+route.get(
+    "/:id",
+    getUsuarioById
+);
+
+route.put(
+    "/:id",
+    updateUsuario
+);
+
+route.delete(
+    "/:id",
+    deleteUsuario
+);
+
+export default route;
